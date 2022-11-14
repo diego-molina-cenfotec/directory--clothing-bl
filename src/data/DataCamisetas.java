bb90a1a4146927136edfb0230b3b12ad228db1eb
diego-molina-cenfotec <dmolinat@ucenfotec.ac.cr>
2022-11-13 22:10:36 -0600

    se trabajo logica de creacion y listado de clientes, sin validaciones.

--- src/data/DataCamisetas.java
+++ src/data/DataCamisetas.java
@@ -1,5 +1,9 @@
 package data;
 
+import modelo.Camiseta;
+import java.util.ArrayList;
+
 public class DataCamisetas {
     // almacenamiento de camisetas
+    private ArrayList camisetas = new ArrayList<Camiseta>();
 }
